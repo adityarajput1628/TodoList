fc37404ed30517417369404bf7f32df5334e811e
adityarajput <115728963+adityarajput1628@users.noreply.github.com>
2023-11-04 23:43:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,37 +2,10 @@ import './App.css';
 import TodoList from './TodoList';
 import TopHeader from './components/TopHeader';
 
-/*import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  link,
-} from "react-router-dom";
-import Home from "./pages/Home"
-import About from './pages/About';
-import ContactUs from './pages/ContactUs';*/
-
-/*const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Home/>
-  },
-  {
-    path: "about",
-    element: <About/>,
-  },
-  {
-    path: "contact-us",
-    element: <ContactUs/>,
-  },
-]);*/
-
-
 function App() {
 
 
   return (
-    /*<RouterProvider router={router} />*/
     <>
     <TodoList/>
       
